8a4dd7d3d9d39f7573449effdf701064e73c2661
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-08-25 20:32:42 -0300

    adiciona projeto

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import {createStore} from 'vuex'
+
+const store = createStore({
+    state() {
+        return {
+            teste: 'Ola mundo',
+            produtosCarrinho: []
+        }
+    },
+    mutations: {
+      alterarTeste(state, value){
+        state.teste = value
+      },
+      adicionarProdutoAoCarrinho(state, value) {
+        state.produtosCarrinho = [...state.produtosCarrinho, value]
+      }
+    },
+    actions: {
+        alterarNome(context, value) {
+            console.log(value.nome)
+            console.log("entrei no alterar nome")
+            context.commit("alterarTeste", value.nome)
+        },
+        adicionarProduto(context, value) {
+            console.log("entrei dentro do adicionarProduto")
+            console.log(value.product)
+            context.commit("adicionarProdutoAoCarrinho", value.product)
+        }
+    }
+})
+
+export default store
---
+++ src/views/Cart/Cart.vue
@@ -0,0 +1,7 @@
+<template>
+    SOu a Cart
+
+    {{this.$store.state.teste}}
+
+     <button @click="this.$store.dispatch('alterarNome', { nome: 'devinhouse'})">Cliquei</button>
+</template>
